baadc23e91dc57a9f9bc3bf64634a1f57d8b8a71
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-10-10 20:37:07 +0330

    ingres: Updating the device.mk with better TWRP Flags

--- device.mk
+++ device.mk
@@ -5,7 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# devices
+TARGET_OTA_ASSERT_DEVICE := ingres
+
 LOCAL_PATH := device/xiaomi/ingres
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -18,18 +22,63 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.xiaomi_sm8450
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+PRODUCT_PACKAGES := \
     bootctrl.xiaomi_sm8450 \
     libgptutils \
     libz \
     libcutils
 
+# FastbootD support
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
 PRODUCT_PACKAGES += \
     otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+    checkpoint_gc
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh 
+
+# TWRP - Crypto
+TW_INCLUDE_CRYPTO               := true
+TW_INCLUDE_CRYPTO_FBE           := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_QCOM_FBE_DECRYPTION  := true
+PLATFORM_VERSION                := 99.87.36
+PLATFORM_VERSION_LAST_STABLE    := $(PLATFORM_VERSION)
+
+# Hardware
+TW_BATTERY_SYSFS_WAIT_SECONDS := 6
+TW_EXCLUDE_DEFAULT_USB_INIT   := true
+TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
+TW_SUPPORT_INPUT_AIDL_HAPTICS_FQNAME := "IVibrator/vibratorfeature"
+TW_SUPPORT_INPUT_AIDL_HAPTICS_FIX_OFF := true
+
+# TWRP - Specifics
+TW_THEME                := portrait_hdpi
+TW_DEFAULT_LANGUAGE     := en
+TW_USE_TOOLBOX          := true
+TW_INCLUDE_NTFS_3G      := true
+TW_INCLUDE_RESETPROP    := true
+TW_INCLUDE_LIBRESETPROP := true
+TW_MAX_BRIGHTNESS       := 2047
+TW_EXTRA_LANGUAGES      := true
+TW_DEFAULT_BRIGHTNESS   := 1020
+TW_EXCLUDE_APEX         := true
+TW_INCLUDE_FASTBOOTD    := true
+TWRP_INCLUDE_LOGCAT     := true
+TW_INCLUDE_PYTHON       := true
+TW_NO_SCREEN_BLANK      := true
+TW_FRAMERATE            := 60
